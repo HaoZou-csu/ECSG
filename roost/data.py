f166033042af8ee1b3fcfa03fb417354cc4b5d54
HaoZou-csu <cs_zouhao@outlook.com>
2024-06-24 15:49:21 +0800

    Add files via upload

---
+++ roost/data.py
@@ -0,0 +1,295 @@
+import functools
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from pymatgen.core import Composition
+
+import sys
+from os.path import dirname, abspath
+path = dirname(dirname(abspath(__file__)))
+sys.path.append(path)
+
+from roost.core import LoadFeaturiser
+
+class CompositionData_SG(Dataset):
+    """
+    The CompositionData dataset is a wrapper for a dataset data points are
+    automatically constructed from composition strings.
+    """
+
+    def __init__(self, df, fea_path, task):
+        """
+        """
+
+        self.df = df
+
+        assert os.path.exists(fea_path), "{} does not exist!".format(fea_path)
+        self.elem_features = LoadFeaturiser(fea_path)
+        self.elem_emb_len = self.elem_features.embedding_size
+        self.task = task
+        if self.task == "regression":
+            if self.df.shape[1] - 2 != 1:
+                raise NotImplementedError(
+                    "Multi-target regression currently not supported"
+                )
+            self.n_targets = 1
+        elif self.task == "classification":
+            if self.df.shape[1] - 2 != 1:
+                raise NotImplementedError(
+                    "One-Hot input not supported please use categorical integer"
+                    " inputs for classification i.e. Dog = 0, Cat = 1, Mouse = 2"
+                )
+            self.n_targets = np.max(self.df[self.df.columns[2]].values) + 1
+
+    def __len__(self):
+        return len(self.df)
+
+    @functools.lru_cache(maxsize=None)  # Cache loaded structures
+    def __getitem__(self, idx):
+        """
+
+        Returns
+        -------
+        atom_weights: torch.Tensor shape (M, 1)
+            weights of atoms in the material
+        atom_fea: torch.Tensor shape (M, n_fea)
+            features of atoms in the material
+        self_fea_idx: torch.Tensor shape (M*M, 1)
+            list of self indices
+        nbr_fea_idx: torch.Tensor shape (M*M, 1)
+            list of neighbor indices
+        target: torch.Tensor shape (1,)
+            target value for material
+        cry_id: torch.Tensor shape (1,)
+            input id for the material
+        """
+        cry_id, composition, target = self.df.iloc[idx]
+        comp_dict = Composition(composition).get_el_amt_dict()
+        elements = list(comp_dict.keys())
+        weights = list(comp_dict.values())
+        weights = np.atleast_2d(weights).T / np.sum(weights)
+        assert len(elements) != 1, f"cry-id {cry_id} [{composition}] is a pure system"
+        try:
+            atom_fea = np.vstack(
+                [self.elem_features.get_fea(element) for element in elements]
+            )
+        except AssertionError:
+            raise AssertionError(
+                f"cry-id {cry_id} [{composition}] contains element types not in embedding"
+            )
+        except ValueError:
+            raise ValueError(
+                f"cry-id {cry_id} [{composition}] composition cannot be parsed into elements"
+            )
+
+        env_idx = list(range(len(elements)))
+        self_fea_idx = []
+        nbr_fea_idx = []
+        nbrs = len(elements) - 1
+        for i, _ in enumerate(elements):
+            self_fea_idx += [i] * nbrs
+            nbr_fea_idx += env_idx[:i] + env_idx[i + 1 :]
+
+        # convert all data to tensors
+        atom_weights = torch.Tensor(weights)
+        atom_fea = torch.Tensor(atom_fea)
+        self_fea_idx = torch.LongTensor(self_fea_idx)
+        nbr_fea_idx = torch.LongTensor(nbr_fea_idx)
+        if self.task == "regression":
+            targets = torch.Tensor([float(target)])
+        elif self.task == "classification":
+            targets = torch.LongTensor([target])
+
+        return (
+            (atom_weights, atom_fea, self_fea_idx, nbr_fea_idx),
+            targets,
+            composition,
+            cry_id,
+        )
+
+
+class CompositionData(Dataset):
+    """
+    The CompositionData dataset is a wrapper for a dataset data points are
+    automatically constructed from composition strings.
+    """
+
+    def __init__(self, data_path, fea_path, task):
+        """
+        """
+        assert os.path.exists(data_path), "{} does not exist!".format(data_path)
+        # NOTE make sure to use dense datasets, here do not use the default na
+        # as they can clash with "NaN" which is a valid material
+        self.df = pd.read_csv(data_path, keep_default_na=False, na_values=[])
+
+        assert os.path.exists(fea_path), "{} does not exist!".format(fea_path)
+        self.elem_features = LoadFeaturiser(fea_path)
+        self.elem_emb_len = self.elem_features.embedding_size
+        self.task = task
+        if self.task == "regression":
+            if self.df.shape[1] - 2 != 1:
+                raise NotImplementedError(
+                    "Multi-target regression currently not supported"
+                )
+            self.n_targets = 1
+        elif self.task == "classification":
+            if self.df.shape[1] - 2 != 1:
+                raise NotImplementedError(
+                    "One-Hot input not supported please use categorical integer"
+                    " inputs for classification i.e. Dog = 0, Cat = 1, Mouse = 2"
+                )
+            self.n_targets = np.max(self.df[self.df.columns[2]].values) + 1
+
+    def __len__(self):
+        return len(self.df)
+
+    @functools.lru_cache(maxsize=None)  # Cache loaded structures
+    def __getitem__(self, idx):
+        """
+
+        Returns
+        -------
+        atom_weights: torch.Tensor shape (M, 1)
+            weights of atoms in the material
+        atom_fea: torch.Tensor shape (M, n_fea)
+            features of atoms in the material
+        self_fea_idx: torch.Tensor shape (M*M, 1)
+            list of self indices
+        nbr_fea_idx: torch.Tensor shape (M*M, 1)
+            list of neighbor indices
+        target: torch.Tensor shape (1,)
+            target value for material
+        cry_id: torch.Tensor shape (1,)
+            input id for the material
+        """
+        cry_id, composition, target = self.df.iloc[idx]
+        comp_dict = Composition(composition).get_el_amt_dict()
+        elements = list(comp_dict.keys())
+        weights = list(comp_dict.values())
+        weights = np.atleast_2d(weights).T / np.sum(weights)
+        assert len(elements) != 1, f"cry-id {cry_id} [{composition}] is a pure system"
+        try:
+            atom_fea = np.vstack(
+                [self.elem_features.get_fea(element) for element in elements]
+            )
+        except AssertionError:
+            raise AssertionError(
+                f"cry-id {cry_id} [{composition}] contains element types not in embedding"
+            )
+        except ValueError:
+            raise ValueError(
+                f"cry-id {cry_id} [{composition}] composition cannot be parsed into elements"
+            )
+
+        env_idx = list(range(len(elements)))
+        self_fea_idx = []
+        nbr_fea_idx = []
+        nbrs = len(elements) - 1
+        for i, _ in enumerate(elements):
+            self_fea_idx += [i] * nbrs
+            nbr_fea_idx += env_idx[:i] + env_idx[i + 1 :]
+
+        # convert all data to tensors
+        atom_weights = torch.Tensor(weights)
+        atom_fea = torch.Tensor(atom_fea)
+        self_fea_idx = torch.LongTensor(self_fea_idx)
+        nbr_fea_idx = torch.LongTensor(nbr_fea_idx)
+        if self.task == "regression":
+            targets = torch.Tensor([float(target)])
+        elif self.task == "classification":
+            targets = torch.LongTensor([target])
+
+        return (
+            (atom_weights, atom_fea, self_fea_idx, nbr_fea_idx),
+            targets,
+            composition,
+            cry_id,
+        )
+
+
+def collate_batch(dataset_list):
+    """
+    Collate a list of data and return a batch for predicting crystal
+    properties.
+
+    Parameters
+    ----------
+
+    dataset_list: list of tuples for each data point.
+      (atom_fea, nbr_fea, nbr_fea_idx, target)
+
+      atom_fea: torch.Tensor shape (n_i, atom_fea_len)
+      nbr_fea: torch.Tensor shape (n_i, M, nbr_fea_len)
+      self_fea_idx: torch.LongTensor shape (n_i, M)
+      nbr_fea_idx: torch.LongTensor shape (n_i, M)
+      target: torch.Tensor shape (1, )
+      cif_id: str or int
+
+    Returns
+    -------
+    N = sum(n_i); N0 = sum(i)
+
+    batch_atom_weights: torch.Tensor shape (N, 1)
+    batch_atom_fea: torch.Tensor shape (N, orig_atom_fea_len)
+        Atom features from atom type
+    batch_self_fea_idx: torch.LongTensor shape (N, M)
+        Indices of mapping atom to copies of itself
+    batch_nbr_fea_idx: torch.LongTensor shape (N, M)
+        Indices of M neighbors of each atom
+    crystal_atom_idx: list of torch.LongTensor of length N0
+        Mapping from the crystal idx to atom idx
+    target: torch.Tensor shape (N, 1)
+        Target value for prediction
+    batch_comps: list
+    batch_ids: list
+    """
+    # define the lists
+    batch_atom_weights = []
+    batch_atom_fea = []
+    batch_self_fea_idx = []
+    batch_nbr_fea_idx = []
+    crystal_atom_idx = []
+    batch_target = []
+    batch_comp = []
+    batch_cry_ids = []
+
+    cry_base_idx = 0
+    for i, (inputs, target, comp, cry_id) in enumerate(dataset_list):
+        atom_weights, atom_fea, self_fea_idx, nbr_fea_idx = inputs
+        # number of atoms for this crystal
+        n_i = atom_fea.shape[0]
+
+        # batch the features together
+        batch_atom_weights.append(atom_weights)
+        batch_atom_fea.append(atom_fea)
+
+        # mappings from bonds to atoms
+        batch_self_fea_idx.append(self_fea_idx + cry_base_idx)
+        batch_nbr_fea_idx.append(nbr_fea_idx + cry_base_idx)
+
+        # mapping from atoms to crystals
+        crystal_atom_idx.append(torch.tensor([i] * n_i))
+
+        # batch the targets and ids
+        batch_target.append(target)
+        batch_comp.append(comp)
+        batch_cry_ids.append(cry_id)
+
+        # increment the id counter
+        cry_base_idx += n_i
+
+    return (
+        (
+            torch.cat(batch_atom_weights, dim=0),
+            torch.cat(batch_atom_fea, dim=0),
+            torch.cat(batch_self_fea_idx, dim=0),
+            torch.cat(batch_nbr_fea_idx, dim=0),
+            torch.cat(crystal_atom_idx),
+        ),
+        torch.stack(batch_target, dim=0),
+        batch_comp,
+        batch_cry_ids,
+    )
